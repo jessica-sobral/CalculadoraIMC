1415c9fd4380bb174089fe232458d052e331ed0c
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-04-06 21:15:17 -0300

    concluido a classe Individuo e os retornos

--- Individuo.cs
+++ Individuo.cs
@@ -1,6 +1,6 @@
 public class Individuo
 {
-    public string Nome { get; }
+    public String Nome { get; }
     public double Altura { get; set; }
     public double Peso { get; set; }
     public double Imc { get; }
@@ -11,35 +11,42 @@ public class Individuo
         Peso = peso;
     }
 
-    public string CalculoIMC(string nome, double altura, double peso, double imc)
+    public double CalculoImc(/*String nome,*/ float altura, float peso)
     {
-        imc = peso / (altura * altura);
+        double imc = peso / (altura * altura);
 
-        return "{0} sua altura é {1} e seu peso é {2}", nome, altura, peso + "\nSeu IMC é {0}", Math.Round(imc, 2);
+        return imc;
     }
 
-    public string ClassificacaoIMC(double imc)
+    public String toString(double imc)
+    {
+        return Nome + " sua altura é " + Math.Round(Altura, 2) + " e seu peso é " + Math.Round(Peso, 2) + ". \nSeu IMC é " + Math.Round(imc, 2) + ".";
+    }
+    
+    public String ClassificacaoImc(double imc)
     {
         if(imc < 18.5)
-            return "Abaixo do peso";
+            return "Abaixo do peso.";
         
         if(imc >= 18.5 && imc <= 24.9)
-            return "Peso normal";
+            return "Peso normal.";
         
         if(imc >= 25 && imc <= 29.9)
-            return "Você está com sobrepeso";
+            return "Sobrepeso.";
         
         if(imc >= 30 && imc <= 34.9)
-            return "Obesidade grau I";
+            return "Obesidade grau I.";
         
         if(imc >= 35 && imc <= 39.9)
-            return "Obesidade grau II";
+            return "Obesidade grau II.";
         
         else if(imc >= 40)
-            return "Obesidade grau III ou mórbida";
-    }
+            return "Obesidade grau III ou mórbida.";
 
-    public void PesoNormal(double altura, double peso, double imc)
+        return "";
+    }
+    
+    public String PesoNormal(double altura, double peso, double imc)
     {
         double pesoNormal = 0.0;
 
@@ -47,14 +54,20 @@ public class Individuo
         {
             pesoNormal = 18.50 * altura * altura;
             double pesoGanhar = pesoNormal - peso;
-            Console.WriteLine("Para alcançar seu peso normal o ideal é ganhar {0} Kg", Math.Round(pesoGanhar, 3));
+
+            return "Para alcançar seu peso ideal, o ideal é ganhar " + Math.Round(pesoGanhar, 3) + " Kg.";
+            //return "Para alcançar seu peso normal o ideal é ganhar {0} Kg", Math.Round(pesoGanhar, 3);
         }
     
         else if(imc >= 24.9)
         {
             pesoNormal = 24.9 * altura * altura;
             double pesoPerder = peso - pesoNormal;
-            Console.WriteLine("Para alcançar seu peso normal o ideal é perder {0} Kg", Math.Round(pesoPerder, 3));
+
+            return "Para alcançar seu peso ideal, o ideal é perder " +  Math.Round(pesoPerder, 3) + " Kg.";
+            //return "Para alcançar seu peso normal o ideal é perder {0} Kg", Math.Round(pesoPerder, 3);
         }
+
+        return "Você já está no seu peso ideal.";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,15 @@
-﻿using System;
+﻿Console.WriteLine("Qual o seu nome?");
+string nome = Console.ReadLine();
 
-namespace CalculadoraIMC
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Qual o seu nome?");
-            string nome = Console.ReadLine();
+Console.WriteLine("Qual a sua altura em metros? (ex.: 1,70)");
+float altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Qual a sua altura em metros? (ex.: 1,70)");
-            float altura = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("Qual o seu peso em Kg? (ex.: 69,2)");
+float peso = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Qual o seu peso em Kg? (ex.: 69,2)");
-            float peso = Convert.ToSingle(Console.ReadLine());
-        }
-    }
-}
+Individuo i = new Individuo(nome, altura, peso);
+double imc = i.CalculoImc(altura, peso);
+
+Console.WriteLine(i.toString(imc));
+Console.WriteLine(i.ClassificacaoImc(imc));
+Console.WriteLine(i.PesoNormal(altura, peso, imc));
